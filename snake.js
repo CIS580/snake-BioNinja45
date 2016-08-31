ce8ddf784823f2379b3b281f8c2009cf381d3d45
Andrew Leigh <Andrew Leigh>
2016-08-31 18:31:55 -0500

    "Snake" created and random "fruits" appearing at 10 second interval. Andrew Leigh

--- snake.js
+++ snake.js
@@ -6,6 +6,19 @@ backBuffer.width = frontBuffer.width;
 backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
+var speed = 1/1000;
+var appleLocations = [];
+var time = 625;
+var snake = ["0","0"];
+var snakeX = 0;
+var snakeY = 0;
+
+var input = {
+	up:false,
+	down:false,
+	left:false,
+	right:false
+}
 
 /**
  * @function loop
@@ -18,12 +31,15 @@ function loop(newTime) {
   update(elapsedTime);
   render(elapsedTime);
 
+  frontCtx.clearRect(0,0,frontBuffer.width,frontBuffer.height);
   // Flip the back buffer
   frontCtx.drawImage(backBuffer, 0, 0);
 
   // Run the next loop
-  window.requestAnimationFrame(loop);
+  //window.requestAnimationFrame(loop);
 }
+var intervalId = setInterval(loop,speed);
+
 
 /**
  * @function update
@@ -35,10 +51,24 @@ function loop(newTime) {
  */
 function update(elapsedTime) {
 	//multiply snake speed by elapsedtime to have same speed across refresh rates
+	// TODO: Spawn an apple periodically
+	var snakeSpeed = speed * elapsedTime;
 	
-  // TODO: Spawn an apple periodically
+	if(time%625 == 0){
+		var height = Math.floor((Math.random() * backBuffer.height) + 1);
+		var width = Math.floor((Math.random() * backBuffer.width) + 1);
+		appleLocations.push([height,width]);
+	}
+	time+=1;
+	
+  
   // TODO: Grow the snake periodically
   // TODO: Move the snake
+	if(input.up)snakeY-=1;
+	if(input.down)snakeY+=1;
+	if(input.left)snakeX-=1;
+	if(input.right)snakeX+=1;
+	
   // TODO: Determine if the snake has moved out-of-bounds (offscreen)
   // TODO: Determine if the snake has eaten an apple
   // TODO: Determine if the snake has eaten its tail
@@ -72,10 +102,62 @@ function update(elapsedTime) {
   * the number of milliseconds passed since the last frame.
   */
 function render(elapsedTime) {
-  backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
+	backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
 
-  // TODO: Draw the game objects into the backBuffer
+	appleLocations.forEach(function(item,index,array){
+	  backCtx.fillStyle = "blue";
+	  backCtx.fillRect(parseInt(item[0]),parseInt(item[1]),5,5);
+	});
+	// TODO: Draw the game objects into the backBuffer
+	backCtx.fillStyle = "#FF0000";
+	backCtx.fillRect(snakeX,snakeY,10,10);
+}
 
+window.onkeydown = function(event)
+{
+	event.preventDefault();
+	switch(event.keyCode)
+	{
+		 case 38:
+		 case 87:
+			input.up = true;
+			break;
+		 case 37:
+		 case 65:
+			input.left = true;
+			break;
+		 case 40:
+		 case 83:
+			input.down = true;
+			break;
+		 case 39:
+		 case 68:
+			input.right = true;
+			break;
+	}
+}
+window.onkeyup = function(event)
+{
+	event.preventDefault();
+	switch(event.keyCode)
+	{
+		 case 38:
+		 case 87:
+			input.up = false;
+			break;
+		 case 37:
+		 case 65:
+			input.left = false;
+			break;
+		 case 40:
+		 case 83:
+			input.down = false;
+			break;
+		 case 39:
+		 case 68:
+			input.right = false;
+			break;
+	}
 }
 
 /* Launch the game */
